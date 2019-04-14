9e5551119e2f7ef39a0f0a8492aa7155d9d23b94
unknown <xliu28@seas.upenn.edu>
2019-04-14 01:00:31 -0500

    update test

--- Dog.java
+++ Dog.java
@@ -60,8 +60,8 @@ public class Dog {
 		
 		//execute code
 		
-		Dog myDog = new Dog("puppy","mixed");
-		myDog.newName = "Pluto";
+		Dog myDog = new Dog("doggy","pink");
+		myDog.newName = "Pinky";
 		
 	}
 }
