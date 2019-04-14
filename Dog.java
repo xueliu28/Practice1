0791d244de482e87ad43da5b6c23131e19196bba
unknown <xliu28@seas.upenn.edu>
2019-04-13 23:14:58 -0500

    Create a simple dog class

---
+++ Dog.java
@@ -0,0 +1,67 @@
+
+public class Dog {
+	
+	String name;
+	String breed;
+	int age;
+	double weight;
+	String newName;
+	
+	public Dog (String name, String breed) {
+		// constructor
+		weight = 125.0;
+		age = 0;
+	}
+	
+	/**
+	 * methods
+	 * @return
+	 */
+	public String getBreed() {
+		return breed;
+	}
+	
+	public String getName() {
+		return name;
+	}
+    
+	public int getAge() {
+		return age;
+	}
+	
+	public double getWeight() {
+		return weight;
+	}
+	
+	
+	public void eat() {
+		// this method increases the weight of the dog by 0.1g
+		weight = Math.round(weight + 0.1);
+	}
+	
+	public void rename(String newName) {
+		/*
+		 *  this method changes the name of the dog to the new name 
+		 *  that is supplied as an argument
+		 */
+		name = newName;
+	}
+	
+	public void hasBirthday() {
+		/*
+		 * print out happy birthday
+		 * increment age of the dog by 1
+		 */
+		System.out.println("Happy birthday");
+		age = age + 1;
+	}
+	
+	public static void main(String[] args) {
+		
+		//execute code
+		
+		Dog myDog = new Dog("puppy","mixed");
+		myDog.newName = "Pluto";
+		
+	}
+}
